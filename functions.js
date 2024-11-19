a89a24a7fe6e42e0df4549c86522b7b7f225fe80
osten-antonio <ostenantonio1@gmail.com>
2024-11-19 22:55:58 +0700

    initial commit

---
+++ functions.js
@@ -0,0 +1,45 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+var accc = document.getElementsByClassName("accordionc");
+var ic;
+
+for (ic = 0; ic < accc.length; ic++) {
+  accc[ic].addEventListener("click", function() {
+
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// TODO: adjust value here
+
+document.querySelector('.leftscroll').addEventListener('click', () => {
+    const wrapper = document.querySelector('.resourceswrapper');
+    wrapper.scrollBy({ left: -600, behavior: 'smooth' }); 
+});
+
+document.querySelector('.rightscroll').addEventListener('click', () => {
+    const wrapper = document.querySelector('.resourceswrapper');
+    wrapper.scrollBy({ left: 600, behavior: 'smooth' }); 
+});
