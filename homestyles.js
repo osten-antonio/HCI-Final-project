7dc6381c0bf93c757efa67e3bbd11aca227481c3
osten-antonio <ostenantonio1@gmail.com>
2024-11-25 17:11:48 +0700

    more stuff

--- homestyles.js
+++ homestyles.js
@@ -32,6 +32,22 @@ for (ic = 0; ic < accc.length; ic++) {
   });
 }
 
+var accd = document.getElementsByClassName("accordiond");
+var id;
+for (id = 0; id < accd.length; id++) {
+  accd[id].addEventListener("click", function() {
+
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 // TODO: adjust value here
 
 document.querySelector('.leftscroll').addEventListener('click', () => {
@@ -43,4 +59,10 @@ document.querySelector('.rightscroll').addEventListener('click', () => {
     const wrapper = document.querySelector('.resourceswrapper');
     wrapper.scrollBy({ left: 600, behavior: 'smooth' }); 
 });
+
+var accd = document.getElementsByClassName("accordiond");
+var id;
+
+
+
 
