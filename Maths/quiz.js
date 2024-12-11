8528c06b913086033bcdc3632cd0eaff12b53bcc
Nixon <nicholasnixon99@yahoo.com>
2024-12-11 14:52:12 +0700

    maths quiz and fuctionality

--- Maths/quiz.js
+++ Maths/quiz.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const loggedInUserId = localStorage.getItem('loggedInUId')
-const mathRef = doc(db,"maths",loggedInUserId)
+const mathRef = doc(db,"math",loggedInUserId)
 const bookmarkRef = doc(db,"bookmarks",loggedInUserId)
 const mathDocSnap = await getDoc(mathRef)
 const bookmarkDocSnap = await getDoc(bookmarkRef)
@@ -25,6 +25,8 @@ var bookmarks = []
 console.log(id)
 
 function getQna(qna,cur){
+    console.log(qna)
+    console.log(cur)
     document.getElementById("questionlabel").innerText = qna[cur].question; 
     document.getElementById("A").innerHTML = qna[cur].answer1;
     document.getElementById("B").innerHTML = qna[cur].answer2;
@@ -34,7 +36,9 @@ function getQna(qna,cur){
 }
 function loadQuestions(){
     if(loggedInUserId){
+        console.log("A")
     if (mathDocSnap.exists() && bookmarkDocSnap.exists()) {
+        console.log("B")
         var change={};
         switch(id){
             case "1":
@@ -42,44 +46,44 @@ function loadQuestions(){
                 var correct = 0;
                 var cur=0;
                 var qna = [{
-                    question:"First question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"9 + 2 =",
+                    answer1:"A. 11",
+                    answer2:"B. 12",
+                    answer3:"C. 13",
+                    answer4:"D. 14",
+                    answer5:"E. 15",
                     correct:"A"},
                     {
-                    question:"second question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"69 - 21 =",
+                    answer1:"A. 46",
+                    answer2:"B. 47",
+                    answer3:"C. 48",
+                    answer4:"D. 49",
+                    answer5:"E. 50",
                     correct:"C"},
                     {
-                    question:"third question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"7 x 4 =",
+                    answer1:"A. 21",
+                    answer2:"B. 28",
+                    answer3:"C. 35",
+                    answer4:"D. 42",
+                    answer5:"E. 49",
                     correct:"B"},
                     {
-                    question:"fourth question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"9 x 9 =",
+                    answer1:"A. 81",
+                    answer2:"B. 90",
+                    answer3:"C. 99",
+                    answer4:"D. 108",
+                    answer5:"E. 117",
                     correct:"A"},
                     {
-                    question:"fifth question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"20 / 5 =",
+                    answer1:"A. 1",
+                    answer2:"B. 2",
+                    answer3:"C. 3",
+                    answer4:"D. 4",
+                    answer5:"E. 5",
                     correct:"D"}
                 ]
 
@@ -100,11 +104,11 @@ function loadQuestions(){
                     updateDoc(bookmarkRef,bookmarkChanges)
                 })
                 document.getElementById("next").addEventListener("click",(event)=>{
-                    if(document.getElementById(qna[cur].correct).checked == True){
+                    if(document.getElementById(qna[cur].correct).checked == true){
                         correct++
                     }
                     cur++
-                    if(cur > 5){
+                    if(cur >= 5){
                         cur--
                         if(window.confirm("Do you want to submit?")){
                             change.mathQuizOne = correct/5 * 100
@@ -131,44 +135,44 @@ function loadQuestions(){
                 var correct = 0;
                 var cur=0;
                 var qna = [{
-                    question:"First question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"The length of a rectangle is 6cm and its height is 4cm. What is the area of the rectangle?",
+                    answer1:"A. 24cm",
+                    answer2:"B. 24m",
+                    answer3:"C. 26cm",
+                    answer4:"D. 3mm",
+                    answer5:"E. 2km",
                     correct:"A"},
                     {
-                    question:"second question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"The length of a the base of a triangle is 5cm and its height is 3cm. What is the area of the triangle?",
+                    answer1:"A. 6.5cm",
+                    answer2:"B. 7cm",
+                    answer3:"C. 7.5cm",
+                    answer4:"D. 8cm",
+                    answer5:"E. 10cm",
                     correct:"C"},
                     {
-                    question:"third question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"The length of a rectangle is 8cm and its height is 6cm. What is the perimiter of the rectangle?",
+                    answer1:"A. 26cm",
+                    answer2:"B. 28cm",
+                    answer3:"C. 30cm",
+                    answer4:"D. 32cm",
+                    answer5:"E. 34cm",
                     correct:"B"},
                     {
-                    question:"fourth question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"The length of one side of a square is 9cm. What is the perimiter of the square?",
+                    answer1:"A. 36cm",
+                    answer2:"B. 45cm",
+                    answer3:"C. 35cm",
+                    answer4:"D. 20cm",
+                    answer5:"E. 50cm",
                     correct:"A"},
                     {
-                    question:"fifth question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"The angle of one acute corner of a right-angle triangle is 57∘. What is the angle of the other acute angle?",
+                    answer1:"A. 12∘",
+                    answer2:"B. 17∘",
+                    answer3:"C. 24∘",
+                    answer4:"D. 33∘",
+                    answer5:"E. 40∘",
                     correct:"D"}
                 ]
 
@@ -189,11 +193,11 @@ function loadQuestions(){
                     updateDoc(bookmarkRef,bookmarkChanges)
                 })
                 document.getElementById("next").addEventListener("click",(event)=>{
-                    if(document.getElementById(qna[cur].correct).checked == True){
+                    if(document.getElementById(qna[cur].correct).checked == true){
                         correct++
                     }
                     cur++
-                    if(cur > 5){
+                    if(cur >= 5){
                         cur--
                         if(window.confirm("Do you want to submit?")){
                             change.mathQuizOne = correct/5 * 100
@@ -221,44 +225,44 @@ function loadQuestions(){
                 var correct = 0;
                 var cur=0;
                 var qna = [{
-                    question:"First question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"Which of the following fractions is equivalent to 2/3?",
+                    answer1:"A. 6/9",
+                    answer2:"B. 4/5",
+                    answer3:"C. 5/7",
+                    answer4:"D. 2/7",
+                    answer5:"E. 3/4",
                     correct:"A"},
-                    {
-                    question:"second question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    {   
+                    question:"Which fraction is the largest?",
+                    answer1:"A. 1/5",
+                    answer2:"B. 7/10",
+                    answer3:"C. 3/4",
+                    answer4:"D. 2/4",
+                    answer5:"E. 3/8",
                     correct:"C"},
                     {
-                    question:"third question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"What is the simplified form of 20/28?",
+                    answer1:"A. 3/4",
+                    answer2:"B. 4/7",
+                    answer3:"C. 5/7",
+                    answer4:"D. 2/3",
+                    answer5:"E. 6/8",
                     correct:"B"},
                     {
-                    question:"fourth question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"Which of the following fractions is equivalent to 3/6?",
+                    answer1:"A. 1/2",
+                    answer2:"B. 1/3",
+                    answer3:"C. 2/3",
+                    answer4:"D. 3/4",
+                    answer5:"E. 5/8",
                     correct:"A"},
                     {
-                    question:"fifth question",
-                    answer1:"A. 1st answer",
-                    answer2:"B. 2nd answer",
-                    answer3:"C. 3rd answer",
-                    answer4:"D. 4th answer",
-                    answer5:"E. 5th answer",
+                    question:"Which fraction is the smallest?",
+                    answer1:"A. 2/3",
+                    answer2:"B. 5/6",
+                    answer3:"C. 1/2",
+                    answer4:"D. 3/8",
+                    answer5:"E. 5/7",
                     correct:"D"}
                 ]
 
@@ -279,11 +283,11 @@ function loadQuestions(){
                     updateDoc(bookmarkRef,bookmarkChanges)
                 })
                 document.getElementById("next").addEventListener("click",(event)=>{
-                    if(document.getElementById(qna[cur].correct).checked == True){
+                    if(document.getElementById(qna[cur].correct).checked == true){
                         correct++
                     }
                     cur++
-                    if(cur > 5){
+                    if(cur >= 5){
                         cur--
                         if(window.confirm("Do you want to submit?")){
                             change.mathQuizOne = correct/5 * 100
